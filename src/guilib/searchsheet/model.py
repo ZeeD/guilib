2e77cff816646120db24b6346441b88782cbd887
Vito De Tullio <vito.detullio@gmail.com>
2025-10-26 17:37:58 +0100

    allow header search

--- src/guilib/searchsheet/model.py
+++ src/guilib/searchsheet/model.py
@@ -1,3 +1,5 @@
+from typing import override
+
 from PySide6.QtCore import QAbstractItemModel
 from PySide6.QtCore import QObject
 from PySide6.QtCore import QSortFilterProxyModel
@@ -12,3 +14,18 @@ class SearchableModel(QSortFilterProxyModel):
         self.setSourceModel(model)
         self.setFilterKeyColumn(-1)
         self.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+
+    @override
+    def setFilterWildcard(self, pattern: str) -> None:
+        if ':' in pattern:
+            key, value = [e.strip() for e in pattern.split(':', 1)]
+            model = self.sourceModel()
+            for column in range(model.columnCount()):
+                header = model.headerData(column, Qt.Orientation.Horizontal)
+                if key == str(header):
+                    self.setFilterKeyColumn(column)
+                    super().setFilterWildcard(value)
+                    return
+
+        self.setFilterKeyColumn(-1)
+        super().setFilterWildcard(pattern)
